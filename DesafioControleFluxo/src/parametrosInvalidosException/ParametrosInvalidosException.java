5dd92ebe95ab64ab2180c6eb6c21e5c51b7c809f
joaopedrojesuss <joaopedrojesusilva@outlook.com>
2024-09-19 02:21:14 -0300

    adicionando desafio ControleFluxo

---
+++ DesafioControleFluxo/src/parametrosInvalidosException/ParametrosInvalidosException.java
@@ -0,0 +1,8 @@
+package parametrosInvalidosException;
+
+public class ParametrosInvalidosException extends Exception {
+	private static final long serialVersionUID = 1L;
+	public ParametrosInvalidosException() {
+		super("O segundo parâmetro deve ser maior que o primeiro");
+	}
+}
